38386ed147860b1ee62118e3c9968784488f17a7
hami.fthi <hami.fthi@gmail.com>
2022-08-21 09:31:15 +0430

    check the conditions explicitly

--- api/api_v1/authentication.py
+++ api/api_v1/authentication.py
@@ -9,9 +9,12 @@ router = APIRouter()
 
 @router.post("/generate-password/", response_model=password.Password)
 async def create_random_password(features: password.PasswordFeatures):
-    # First we check if all option were None, then raise exception
-    if any(list(features.dict().values())) is False:
-        raise HTTPException(status_code=400, detail="All values couldn't be null. At least one option should be set.")
+    # First we check if all option were None or all conditions were false, then raise exception
+    conditions = [features.include_numbers, features.include_lowercase_chars,
+                  features.include_uppercase_chars, features.include_special_chars]
+    if any(list(features.dict().values())) is False or any(conditions) is False:
+        raise HTTPException(status_code=400, detail="All values couldn't be null or false. At least"
+                                                    " one option should be set.")
     # Add input data to a dict and add default values from settings in case of None values
     password_features = dict()
     for key, value in features.dict().items():
